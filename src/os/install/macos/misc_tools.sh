80533d1a6751ae20fd9dc68742afec8298862dca
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2019-11-03 00:06:36 +0100

    Setting up everything

--- src/os/install/macos/misc_tools.sh
+++ src/os/install/macos/misc_tools.sh
@@ -9,6 +9,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Miscellaneous Tools\n\n"
 
 brew_install "ShellCheck" "shellcheck"
+brew_install "Fzf" "fzf"
 
 if [ -d "$HOME/.nvm" ]; then
     brew_install "Yarn" "yarn"
