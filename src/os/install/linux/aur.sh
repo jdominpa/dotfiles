7c8ff28bb1e5a4e9ddc7c95dfc254d6b68ddc369
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2020-03-25 22:00:01 +0100

    Dracula gtk theme added

--- src/os/install/linux/aur.sh
+++ src/os/install/linux/aur.sh
@@ -11,3 +11,4 @@ print_in_purple "\n   AUR programs\n\n"
 # Status bar
 install_aur_package "Polybar" "polybar"
 install_aur_package "Brave" "brave-bin"
+install_aur_package "Ant gtk theme" "ant-gtk-theme"
