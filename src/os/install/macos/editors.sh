c431128c0be8e268430c22187e9ca026546535a4
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2020-05-06 23:12:19 +0200

    Solved emacs-plus install errors

--- src/os/install/macos/editors.sh
+++ src/os/install/macos/editors.sh
@@ -6,13 +6,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+print_in_purple "\n   Text editors\n\n"
+
 brew_install "Neovim" "neovim"
 
-brew_install "Emacs" "emacs-plus --without-spacemacs-icon" "d12frosted/emacs-plus"
-execute "brew linkapps emacs-plus" \
-        "Set Emacs.app symlink"
-#execute "ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app" \
-#    "Setting emacs.app symlink"
+brew_install "Emacs" "emacs-plus" "d12frosted/emacs-plus"
+execute "ln -sf /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app" \
+	"Setting Emacs.app symlink"
 brew_install "Ripgrep" "ripgrep"
 
 #brew_install "Doom-emacs dependencies" "ripgrep fd"
