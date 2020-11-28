20a29d8c7a71a68e92a30d89d71d33ca78107671
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2020-11-28 12:44:30 +0100

    Small adjustments to lsp-mode

--- src/emacs/emacs.d/modules/init-lsp.el
+++ src/emacs/emacs.d/modules/init-lsp.el
@@ -13,7 +13,11 @@
 
 (use-package lsp-ui
   :hook (lsp-mode . lsp-ui-mode)
-  :config (setq lsp-ui-doc-position 'bottom))
+  :config
+  (setq lsp-ui-doc-position 'bottom
+        lsp-ui-doc-alignment 'window
+        lsp-ui-doc-show-with-cursor nil
+        lsp-ui-sideline-show-code-actions nil))
 
 (use-package lsp-ivy
   :after lsp)
