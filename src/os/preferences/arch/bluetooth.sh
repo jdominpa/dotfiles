61e5e9d7f853151703a570ef9872e1ac87a0c9a9
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2020-12-09 23:13:13 +0100

    Arch: changing color theme to palenight

---
+++ src/os/preferences/arch/bluetooth.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Bluetooth activation\n\n"
+
+execute "sudo systemctl enable --now bluetooth.service" \
+    "Bluez"
