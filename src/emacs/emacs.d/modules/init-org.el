722c6ff7b63740b2540234205f5ac7a668cf1483
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2020-12-25 22:22:51 +0100

    Emacs: removed unused settings in org-mode
    I've removed org-bullets package since I don't like it and I've also
    removed the different header sizes. Some minor changes have also been
    made to the rest of org-mode config.

--- src/emacs/emacs.d/modules/init-org.el
+++ src/emacs/emacs.d/modules/init-org.el
@@ -8,38 +8,13 @@
 
 (use-package org
   :ensure org-plus-contrib
-  :preface
-  (defun jdp/org-mode-setup ()
-    (org-indent-mode)
-    (visual-line-mode 1)
-    (auto-fill-mode 1))
-  (defun jdp/org-font-setup ()
-    "Set faces for 'org-mode' heading levels."
-    (dolist (face '((org-level-1 . 1.2)
-                    (org-level-2 . 1.1)
-                    (org-level-3 . 1.05)
-                    (org-level-4 . 1.0)
-                    (org-level-5 . 1.0)
-                    (org-level-6 . 1.0)
-                    (org-level-7 . 1.0)
-                    (org-level-8 . 1.0)))
-      (set-face-attribute (car face) nil :font "monospace" :weight 'regular :height (cdr face))))
   :hook ((org-mode . eldoc-mode)
-         (org-mode . jdp/org-mode-setup)
-         (org-mode . jdp/org-font-setup))
+         (org-mode . org-indent-mode)
+         (org-mode . auto-fill-mode))
   :config
   (setq org-ellipsis " ▾")
-
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((emacs-lisp . t)))
+  (org-babel-do-load-languages 'org-babel-load-languages '((emacs-lisp . t)))
   (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp\n")))
-
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :config
-  (setq org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 
 (provide 'init-org)
