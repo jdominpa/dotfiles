3c533a7f66c6021ec25f9e8467d4d7fb7a762a0d
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2021-03-02 15:32:11 +0100

    Emacs: removed use-package for configuring python-mode

--- src/emacs/emacs.d/modules/init-python.el
+++ src/emacs/emacs.d/modules/init-python.el
@@ -7,9 +7,7 @@
 
 (require 'init-programming)
 
-(use-package python-mode
-  :ensure nil
-  :config (setq python-shell-interpreter "python3"))
+(setq python-shell-interpreter "python3")
 
 (use-package lsp-pyright
   :hook (python-mode . lsp-deferred))
