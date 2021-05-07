4a8b8cef11a21e2bfc69b4e047c3e3ae3babc331
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2021-05-07 23:13:07 +0200

    macOS: changed to emacs-plus@28

--- setup/install/macos/editors.sh
+++ setup/install/macos/editors.sh
@@ -10,8 +10,7 @@ print_in_purple "\n   Text editors\n\n"
 
 brew_install "Neovim" "neovim"
 
-# Trying out default Emacs package for macOS
-# brew_install "Emacs" "emacs-plus" "d12frosted/emacs-plus"
-# execute "ln -sf /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app" \
-#   "Setting Emacs.app symlink"
-brew_install "Emacs" "emacs" "homebrew/cask" "--cask"
+brew_install "Emacs" "emacs-plus@28" "d12frosted/emacs-plus"
+execute "ln -sf /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app" \
+  "Setting Emacs.app symlink"
+# brew_install "Emacs" "emacs" "homebrew/cask" "--cask"
