137ccaf93e3ab357a6c219d6ac7a541460955b16
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2021-05-07 17:04:10 +0200

    Emacs: minibuffer completion improvements

--- stow/emacs/.emacs.d/jdp-lisp/jdp-minibuffer.el
+++ stow/emacs/.emacs.d/jdp-lisp/jdp-minibuffer.el
@@ -19,7 +19,8 @@
 
 ;;; Commentary:
 
-;; Extensions for the minibuffer used in my Emacs configuration
+;; Extensions for the minibuffer used in my Emacs configuration.
+;; Almost everything is copied from https://protesilaos.com/dotemacs/.
 
 ;;; Code:
 
@@ -28,6 +29,13 @@
 (defgroup jdp-minibuffer ()
   "Extensions for the minibuffer."
   :group 'minibuffer)
+
+(defcustom jdp-minibuffer-completion-windows-regexp
+  "\\*\\(Completions\\|Embark Collect \\(Live\\|Completions\\)\\)"
+  "Regexp to match window names with completion candidates.
+Used by `jdp-minibuffer--get-completion-window'."
+  :group 'jdp-minibuffer
+  :type 'string)
 
 (defcustom jdp-minibuffer-remove-shadowed-file-names nil
   "Delete shadowed parts of file names.
@@ -79,6 +87,8 @@ delay introduced by `jdp-minibuffer-live-update-delay'."
 
 ;;;; Minibuffer behaviour
 
+;; Thanks to Omar Antolín Camarena for providing the messageless and
+;; stealthily.  Source: https://github.com/oantolin/emacs-config.
 (defun jdp-minibuffer--messageless (fn &rest args)
   "Set `minibuffer-message-timeout' to 0.
 Meant as advice around minibuffer completion FN with ARGS."
@@ -156,7 +166,80 @@ To be assigned to `minibuffer-setup-hook'."
     (when mini
       (select-window mini))))
 
+(defun jdp-minibuffer--get-completion-window ()
+  "Find a live window showing completion candidates."
+  (get-window-with-predicate
+   (lambda (window)
+     (string-match-p
+      jdp-minibuffer-completion-windows-regexp
+      (format "%s" window)))))
+
+;; Adaptation of `icomplete-fido-backward-updir'.
+;;;###autoload
+(defun jdp-minibuffer-backward-updir ()
+  "Delete char before point or go up a directory.
+Must be bound to `minibuffer-local-filename-completion-map'."
+  (interactive)
+  (if (and (eq (char-before) ?/)
+           (eq (jdp-minibuffer--completion-category) 'file))
+      (save-excursion
+        (goto-char (1- (point)))
+        (when (search-backward "/" (point-min) t)
+          (delete-region (1+ (point)) (point-max))))
+    (call-interactively 'backward-delete-char)))
+
 ;;;; Minibuffer and Completions' buffer intersection
+
+(defface jdp-minibuffer-hl-line
+  '((default :extend t)
+    (((class color) (min-colors 88) (background light))
+     :background "#b0d8ff" :foreground "#000000")
+    (((class color) (min-colors 88) (background dark))
+     :background "#103265" :foreground "#ffffff")
+    (t :inherit (font-lock-string-face elfeed-search-title-face)))
+  "Face for current line in the completions' buffer."
+  :group 'jdp-minibuffer)
+
+(defface jdp-minibuffer-line-number
+  '((default :inherit default)
+    (((class color) (min-colors 88) (background light))
+     :background "#f2eff3" :foreground "#252525")
+    (((class color) (min-colors 88) (background dark))
+     :background "#151823" :foreground "#dddddd")
+    (t :inverse-video t))
+  "Face for line numbers in the completions' buffer."
+  :group 'jdp-minibuffer)
+
+(defface jdp-minibuffer-line-number-current-line
+  '((default :inherit default)
+    (((class color) (min-colors 88) (background light))
+     :background "#8ac7ff" :foreground "#000000")
+    (((class color) (min-colors 88) (background dark))
+     :background "#142a79" :foreground "#ffffff")
+    (t :inverse-video t))
+  "Face for current line number in the completions' buffer."
+  :group 'jdp-minibuffer)
+
+(autoload 'display-line-numbers-mode "display-line-numbers")
+(autoload 'face-remap-remove-relative "face-remap")
+
+;;;###autoload
+(defun jdp-minibuffer-display-line-numbers ()
+  "Set up line numbers for the completions' buffer.
+Add this to `completion-list-mode-hook'."
+  (when (derived-mode-p 'completion-list-mode)
+    (face-remap-add-relative 'line-number 'jdp-minibuffer-line-number)
+    (face-remap-add-relative 'line-number-current-line
+                             'jdp-minibuffer-line-number-current-line)
+    (display-line-numbers-mode 1)))
+
+;;;###autoload
+(defun jdp-minibuffer-hl-line ()
+  "Set up line highlighting for the completions' buffer.
+Add this to `completion-list-mode-hook'."
+  (when (derived-mode-p 'completion-list-mode)
+    (face-remap-add-relative 'hl-line 'jdp-minibuffer-hl-line)
+    (hl-line-mode 1)))
 
 (defun jdp-minibuffer--clean-completions ()
   "Keep only completion candidates in the Completions."
@@ -294,7 +377,8 @@ minibuffer."
           (eq (point-max)
               (save-excursion (forward-line 1) (point))))
       (jdp-minibuffer-focus-minibuffer)
-    (next-completion (or arg 1))))
+    (next-completion (or arg 1)))
+  (setq this-command 'next-line))
 
 ;;;###autoload
 (defun jdp-minibuffer-previous-completion-or-mini (&optional arg)
@@ -308,6 +392,36 @@ minibuffer."
             (eq (point) (1+ (point-min)))) ; see hack in `jdp-minibuffer--clean-completions'
         (jdp-minibuffer-focus-minibuffer)
       (next-completion (or num 1)))))
+
+(defun jdp-minibuffer--goto-line (n &optional args)
+  "Go to line N in the Completions' with optional ARGS."
+  (let ((bounds (count-lines (point-min) (point-max))))
+    (if (<= n bounds)
+        (progn
+          `(,@args)
+          (goto-char (point-min))
+          (forward-line (1- n))
+          (choose-completion))
+      (user-error "%d is not within Completions' buffer bounds (%d)" n bounds))))
+
+;;;###autoload
+(defun jdp-minibuffer-choose-completion-number (n)
+  "Select completion candidate on line number N with prefix arg.
+
+The idea is to pass a prefix numeric argument that refers to a
+line number in the Completions' buffer."
+  (interactive "p")
+  (if current-prefix-arg
+      (cond
+       ((and (derived-mode-p 'completion-list-mode)
+             (active-minibuffer-window))
+        (jdp-minibuffer--goto-line n))
+       ((and (minibufferp)
+             (jdp-minibuffer--get-completion-window))
+        (jdp-minibuffer--goto-line n (select-window (jdp-minibuffer--get-completion-window))))
+       (t
+        (user-error "Only use this inside the minibuffer of the Completions")))
+    (user-error "Pass a numeric argument first")))
 
 (provide 'jdp-minibuffer)
 ;;; jdp-minibuffer.el ends here
