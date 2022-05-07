2d3f66762667f7d5827ba2311ca41569256d493b
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2022-05-07 16:19:47 +0200

    Nvim: improvements to snippet config

--- stow/neovim/.config/nvim/after/plugin/luasnip.lua
+++ stow/neovim/.config/nvim/after/plugin/luasnip.lua
@@ -2,6 +2,79 @@ if vim.g.snippets ~= "luasnip" or not pcall(require, "luasnip") then
   return
 end
 
+-- Snippet configuration
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local events = require("luasnip.util.events")
+local fmt = require("luasnip.extras.fmt").fmt
+
+ls.config.set_config {
+  history = true,
+  updateevents = "TextChanged,TextChangedI",
+  enable_autosnippets = true,
+}
+
+-- Helper functions
+_G.if_char_insert_space = function()
+  if string.find(vim.v.char, "%a") then
+    vim.v.char = " " .. vim.v.char
+  end
+end
+
+local tex = {}
+
+tex.in_mathzone = function()
+  return vim.fn['vimtex#syntax#in_mathzone']() == 1
+end
+
+tex.in_text = function()
+  return not tex.in_mathzone
+end
+
+tex.rec_ls = function()
+  return sn(nil, {
+    c(1, {
+      t({""}),
+      sn(nil, {t({"", "\t\\item "}), i(1), d(2, tex.rec_ls, {})}),
+    }),
+  });
+end
+
+ls.add_snippets("tex", {
+
+  s("dm", {
+    t({ "\\[", "\t" }),
+    i(1),
+    t({ "", "\\]" }),
+  }, { condition = tex.in_text }),
+
+  s("mk", { t("$"), i(1), t("$") }, {
+    callbacks = {
+      [-1] = {
+        [events.leave] = function()
+          vim.cmd([[
+            autocmd InsertCharPre <buffer> ++once lua _G.if_char_insert_space()
+          ]])
+        end
+      }
+    }
+  }),
+
+  s("ls", {
+    t({ "\\begin{itemize}", "\t\\item " }),
+    i(1),
+    d(2, tex.rec_ls, {}),
+    t({ "", "\\end{itemize}" }),
+  }),
+
+})
+
 -- Keybindings
 vim.keymap.set({ "i", "s" }, "<C-j>", function()
   if ls.expand_or_jumpable() then
@@ -22,31 +95,3 @@ vim.keymap.set("i", "<C-l>", function()
 end)
 
 vim.keymap.set("n", "<leader><leader>s", "<cmd>source $HOME/.config/nvim/after/plugin/luasnip.lua<CR>")
-
--- Snippet configuration
-local ls = require("luasnip")
-
-local s = ls.snippet
-local sn = ls.snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
-local fmt = require("luasnip.extras.fmt").fmt
-
-ls.config.set_config {
-  history = true,
-  updateevents = "TextChanged,TextChangedI",
-  enable_autosnippets = true,
-}
-
-ls.add_snippets("all", {
-  s("dm",
-    fmt([[
-      \[
-      {}
-      \]
-      {}]],
-      { i(1), i(0), }))
-})
