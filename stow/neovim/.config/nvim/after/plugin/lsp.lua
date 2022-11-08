9aea5f1e4ea096109776488cd7d779a90e3bb0bc
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2022-11-08 13:00:53 +0100

    Tide up lsp keybindings

--- stow/neovim/.config/nvim/after/plugin/lsp.lua
+++ stow/neovim/.config/nvim/after/plugin/lsp.lua
@@ -1,19 +1,21 @@
+local keymap = require("jdominpa.keymap")
+local nnoremap = keymap.nnoremap
+
 -- LSP keymappings
 local on_attach = function(client, bufnr)
-  local keymap = vim.keymap.set
-  local opts = { noremap = true, silent = true, buffer = bufnr }
+  local opts = { buffer = bufnr }
 
-  keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  keymap("n", "gT", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  keymap("n", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  keymap("n", "<localleader>lr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  keymap("n", "<localleader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-  keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+  nnoremap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  nnoremap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  nnoremap("gT", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  nnoremap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  nnoremap("gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  nnoremap("K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  nnoremap("<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  nnoremap("<localleader>lr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  nnoremap("<localleader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  nnoremap("[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+  nnoremap("]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
