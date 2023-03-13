68c0289518d37a737da65cfbb56a88474c5d3a95
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2023-03-13 11:45:36 +0100

    Added linebreaking to latex files

---
+++ stow/config/.config/nvim/after/ftplugin/tex.lua
@@ -0,0 +1,5 @@
+vim.opt_local.conceallevel = 1
+vim.opt_local.formatoptions = vim.opt_local.formatoptions + 't'
+vim.opt_local.textwidth = 80
+vim.g.tex_conceal = 'abdmgs'
+vim.g.tex_flavor = 'latex'
--- stow/config/.config/nvim/after/ftplugin/tex.vim
+++
@@ -1,3 +0,0 @@
-setlocal conceallevel=1
-let g:tex_conceal='abdmgs'
-let g:tex_flavor='latex'
